60f9967defeeb153c942d1b2a79484c7d222cbc5
NikVolf <nikvolf@gmail.com>
2020-10-30 16:31:13 +0300

    add free and use external

--- src/wasm.rs
+++ src/wasm.rs
@@ -1,9 +1,16 @@
 use core::arch::wasm32;
 use core::ptr;
 
+mod sys {
+    extern "C" {
+        pub fn alloc(pages: u32) -> usize;
+        pub fn free(page: u32);
+    }
+}
+
 pub unsafe fn alloc(size: usize) -> (*mut u8, usize, u32) {
     let pages = size / page_size();
-    let prev = wasm32::memory_grow(0, pages);
+    let prev = sys::alloc(pages as _);
     if prev == usize::max_value() {
         return (ptr::null_mut(), 0, 0);
     }
@@ -19,8 +26,16 @@ pub unsafe fn free_part(_ptr: *mut u8, _oldsize: usize, _newsize: usize) -> bool
     false
 }
 
-pub unsafe fn free(_ptr: *mut u8, _size: usize) -> bool {
-    false
+pub unsafe fn free(ptr: *mut u8, size: usize) -> bool {
+    let mut pages = size / page_size();
+    if size % page_size() > 0 { pages += 1; }
+    let first_page = ptr as usize / page_size();
+
+    for page in 0..pages {
+        sys::free(page as _)
+    }
+
+    true
 }
 
 pub fn can_release_part(_flags: u32) -> bool {
