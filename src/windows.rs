a41add69b2f2da3863835bd73af6d62f27f92e7d
Gregory Sobol <grishasobol@mail.ru>
2022-02-25 13:26:22 +0300

    PR fixes

--- src/windows.rs
+++ src/windows.rs
@@ -1,27 +1,30 @@
+//! Windows is unsupported currently.
+//! It means, that you cannot use this allocator in native windows programs.
+
 pub fn page_size() -> usize {
-    unreachable!("unsupport windows");
+    unreachable!("windows is unsupported");
 }
 
 pub unsafe fn get_preinstalled_memory() -> (usize, usize) {
-    unreachable!("unsupport windows");
+    unreachable!("windows is unsupported");
 }
 
 pub unsafe fn alloc(size: usize) -> (*mut u8, usize, u32) {
-    unreachable!("unsupport windows");
+    unreachable!("windows is unsupported");
 }
 
 pub unsafe fn free(ptr: *mut u8, size: usize) -> bool {
-    unreachable!("unsupport windows");
+    unreachable!("windows is unsupported");
 }
 
 pub use crate::common::get_free_borders;
 
 #[cfg(feature = "global")]
 pub fn acquire_global_lock() {
-    unreachable!("unsupport windows");
+    unreachable!("windows is unsupported");
 }
 
 #[cfg(feature = "global")]
 pub fn release_global_lock() {
-    unreachable!("unsupport windows");
+    unreachable!("windows is unsupported");
 }
